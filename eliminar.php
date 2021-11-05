0f2fb30a7d42319e555f324d6ca8e063391e7041
mariajoseluva <50151788+mariajoseluva@users.noreply.github.com>
2021-11-04 20:03:00 -0700

    Update eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -7,8 +7,7 @@ $pdo = new PDO('mysql:host=localhost;dbname=' . $dbname, $username, $password);
 if (isset($_POST['colum1'])) {
   $id=$_POST['colum1'];
   $id=$_POST['colum1'];
-  $query = "DELETE FROM table1 WHERE column1=:id;";
-  $query1 = "DELETE FROM table1 WHERE column1=:id;";
+  $query = "DELETE FROM table1 WHERE column1=:id;"
  
   $stmt = $pdo->prepare($query);
   $stmt->bindParam(":id", $id);
